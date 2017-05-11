1d344112e10a891ae726dc5c708f3553e35731ba
user <mramirez@usa.com>
2017-05-11 15:27:15 -0500

    added format printf  to exercises

---
+++ src/io/StringExercise.java
@@ -0,0 +1,41 @@
+package io;
+
+import java.util.Scanner;
+
+
+/**
+ * Created by user on 5/11/17.
+ */
+public class StringExercise {
+
+    public static void main (String[] args){
+
+// //Exer#1.. use this sentence: We don't need no -- education <--take off education..add thought control
+//......... new result should be -> We don't need no -- thought control
+//
+//        String message = "We don't need no education ";
+//
+//            System.out.println(message);
+//
+//        message = message.replace("need", "want");
+//        System.out.println(message);
+//        // or
+//        System.out.println(message.replace("need", "yes"));
+//
+// // Exerc 2=====================
+//        String message="We don't need no education\nwe don't need no thought control\n \nCheck \"this\"out!, \"s inside " +
+//                "of \"s!\n \n In windows, the main drive is usually C:\\\n I can do backslashes \\, double " +
+//                "backslashes \\\\,\n and the amazing triple backslash \\\\\\!  ";
+//
+//        System.out.println(message);
+
+//exer 3=======================
+
+        Scanner input =new Scanner(System.in);
+        System.out.println("talk to Bob");
+        String message = input.nextLine();
+
+
+
+    } //end pub stat voi main (Str[] args)
+} // end pub class
