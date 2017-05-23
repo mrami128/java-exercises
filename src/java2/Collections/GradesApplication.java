9fd038d05b626cf41cf4dbb1869d0966ea9b9e41
user <mramirez@usa.com>
2017-05-23 18:54:03 -0500

    make hash practice file

--- src/java2/Collections/GradesApplication.java
+++ src/java2/Collections/GradesApplication.java
@@ -1,23 +1,56 @@
 package java2.Collections;
 
-
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class GradesApplication {
 
     public static void main(String[] args) {
-        Map<Integer, Object> map = new HashMap<>();
+      String yesNo;   // relates to do you want to see another studen
 
+      Map<String, Student> students = new HashMap<>();
+
+        Student student1 = new Student("Fernando ");
+        student1.addGrade(80);
+        student1.addGrade(90);
+
+        Student student2 = new Student("Justin ");
+        student2.addGrade(100);
+        student2.addGrade(78);
+        student2.addGrade(90);
+
+        students.put("fernando", student1);
+        students.put("justin", student2);
+
+// ? where to start and end the   do / while loop --------
+do{
+        System.out.println("Welcome!");
+        System.out.println(" here are the github username of students:");
+
+        students.forEach((username, student) -> System.out.print(" | " + username + " | "));
+
+    System.out.println("\nenter a student username:");
+        Scanner input = new Scanner(System.in);
+        String username = input.next();
+
+
+        if (!students.containsKey(username)) {              // this looks for bool to see if true or false
+
+            System.out.println("Sorry no student found with this name. ");
+
+        } else {
+            Student student = students.get(username);
+            System.out.println("\nname: " + student.getName() + " --> git user name is: " + username);
+            System.out.println("curr avg : " + student.getGradeAverage());
+        }
+    System.out.println("\nWould yu like to see another student y-n");
+    yesNo = input.next();
+
+} while ("y".equalsIgnoreCase(yesNo));
+        System.out.println("Goodbye have good day");
 
     }//end psvm
-
-
-
-
-
-
-
 
 } // end class gradesApplication
 
--- src/java2/Collections/Student.java
+++ src/java2/Collections/Student.java
@@ -1,8 +1,6 @@
 package java2.Collections;
 
-import java.util.Scanner;
 import java.util.ArrayList;
-
 
 public class Student {
 
@@ -11,10 +9,10 @@ public class Student {
 
     public Student(String name) {          // this is constructor --so Must be public
         this.name = name;
-        this.grades = new ArrayList<>();  //
+        this.grades = new ArrayList<>();  // setting up ArrayList
     }
 
-    // returns the student's name //------------------
+    // returns the student's name //------------------getter
     public String getName() {
         return name;          // can return null if dont know what to return at first
     }
@@ -22,71 +20,11 @@ public class Student {
     // add the given grade to the grades property //-------------
     public void addGrade(int grade) {  grades.add(grade); }             //adding grade to the student
 
-//        System.out.println();
-//        for (Integer num: grades) {
-//            System.out.println(num);
-//        }
 
-
-    // returns the average of the students grades
     public double getGradeAverage() {
 
         int sum =0;
-        for (Integer grade:grades) {sum+=grade;} return (double)sum /grades.size();}
+        for (Integer grade:grades) { sum += grade; } return (double)sum /grades.size();}
 
-//        int entrySize = grades.size();
-//        int gradeEntries = 0;
-//        double average = 0;
-//
-//        for (Integer num : grades) {
-//            gradeEntries += num;
-//        }
-//        average = gradeEntries / entrySize;
-//        return average;
-//        }
-//----------------------------------------------
-
-    public static void main(String[] args) {
-    //--------
-     Student student1 =new Student("zach");
-        student1.addGrade(100);
-        student1.addGrade(90);
-        student1.addGrade(80);
-        System.out.println("student 1 avg : " + student1.getGradeAverage());
-
-     Student student2 =new Student("ben");
-        student2.addGrade(100);
-        student2.addGrade(90);
-        student2.addGrade(80);
-        System.out.println("\nstudent 2 avg : " + student2.getGradeAverage());
-
-
-        //-------
-        String yesNo;
-
-
-            Scanner sc = new Scanner(System.in);
-
-            System.out.println("Enter student name:");
-                String name = sc.nextLine();
-                Student student = new Student(name);
-
-        do {
-            System.out.println("Enter students grade: ");
-            int grade = sc.nextInt();
-            student.addGrade(grade);
-
-            sc.nextLine();
-            System.out.println("Enter another grade?");
-            yesNo = sc.nextLine();
-
-        } while (yesNo.equalsIgnoreCase("Y"));
-
-        System.out.println(name);                       //Why do both of these work?
-        System.out.println(student.getName());
-        System.out.println(student.getGradeAverage());
-
-
-    } // end psvm main
 
 } //end class student
